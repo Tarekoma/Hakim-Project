9f4c9f2b33f52064a9b5d811983fe0d6921f1d89
Tarek Omar <162827740+Tarekoma@users.noreply.github.com>
2026-03-06 15:36:03 +0200

    Add files via upload

---
+++ lib/main/main.dart
@@ -0,0 +1,21 @@
+import 'package:Hakim/api_service.dart';
+import 'package:flutter/material.dart';
+import 'package:Hakim/splash_screen.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  ApiService.init();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: const SplashScreen(), // ← changed from LoginPage
+    );
+  }
+}
